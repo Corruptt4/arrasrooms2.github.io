303811958622a40637b27a7199d2fb3169c5ac06
Glitch (invented-indigo-giant) <none>
2022-05-29 21:52:03 +0000

    🎰🎪 Checkpoint ./server/permissions.json:21793105/2 ./server/lib/definitions.js:21793105/1115

--- server/lib/definitions.js
+++ server/lib/definitions.js
@@ -1770,6 +1770,40 @@ exports.testbed8 = {
                 }, }, 
             ],
         };
+
+exports.testbed9 = {
+            PARENT: [exports.genericTank],
+            LABEL: 'Custom Bosses',
+            RESET_UPGRADES: true,
+            SKILL: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+            LEVEL: -1,
+          SKILL_CAP: [dfltskl, dfltskl, dfltskl, dfltskl, dfltskl, dfltskl, dfltskl, dfltskl, dfltskl, dfltskl],
+          INVISIBLE: [],
+    BODY: {
+        ACCELERATION: 1,
+        SPEED: 0,
+        HEALTH: 1,
+        RESIST: 1,
+        SHIELD: 0,
+        REGEN: 0,
+        DAMAGE: 1,
+        PENETRATION: 1,
+        RANGE: 0,
+        FOV: 2,
+        DENSITY: 1,
+        STEALTH: 1,
+        PUSHABILITY: 1,        
+        HETERO: 2,
+    },    
+            TURRETS: [],
+            GUNS: [ { /*** LENGTH  WIDTH   ASPECT    X       Y     ANGLE   DELAY */
+                POSITION: [  18,    10,    -1.4,     0,      0,      0,      0,   ], 
+                PROPERTIES: {
+                    SHOOT_SETTINGS: combineStats([g.basic, g.op]),
+                    TYPE: exports.bullet,
+                }, }, 
+            ],
+        };
             exports.single = {
                 PARENT: [exports.genericTank],
                 LABEL: 'Single',
@@ -6859,14 +6893,14 @@ exports.testbed.UPGRADES_TIER_1 = [exports.basic, exports.betatester, exports.te
 exports.betatester.UPGRADES_TIER_1 = [exports.basic,exports.supertest,exports.indust,exports.miner,exports.imposter,exports.nap,exports.furnace,exports.dumptruck,exports.exploder,exports.balli,exports.gen,exports.scattergun,exports.lancer,exports.archer];
 exports.lancer.UPGRADES_TIER_2 = [exports.trilance,exports.knife,exports.sword,exports.invislancer];
 exports.testbed2.UPGRADES_TIER_1 = [exports.arenaCloser, exports.mothership, exports.dominator, exports.dominationBody, exports.destroyerDominator, exports.gunnerDominator, exports.trapperDominator]
-exports.testbed3.UPGRADES_TIER_1 = [exports.elite_destroyer, exports.elite_gunner, exports.elite_sprayer, exports.elite_battleship, exports.palisade, exports.skimboss, exports.summoner, exports.nestKeeper, exports.centre];
+exports.testbed3.UPGRADES_TIER_1 = [exports.elite_destroyer, exports.elite_gunner, exports.elite_sprayer, exports.elite_battleship, exports.palisade, exports.skimboss, exports.summoner, exports.nestKeeper, exports.testbed9];
 exports.testbed4.UPGRADES_TIER_1 = [exports.hugePentagon, exports.bigPentagon, exports.pentagon, exports.triangle, exports.square, exports.egg, exports.greenpentagon, exports.gem, exports.obstacle, exports.babyObstacle, exports.crasher, exports.trapTurret, exports.sentrySwarm,exports.testbed6]
 exports.testbed5.UPGRADES_TIER_1 = [exports.weenus,exports.ball,exports.bender,exports.omega,exports.dogeTest, exports.mazeWallShooter]
 exports.testbed6.UPGRADES_TIER_1 = [exports.greenpentagon, exports.greentriangle, exports.greensquare]
 //exports.testbed7.UPGRADES_TIER_1 = [exports.bullet, exports.casing, exports.flare, exports.swarm, exports.bee, exports.autoswarm, exports.homingbullet, exports.accelbullet, exports.growbullet, exports.trap, exports.block, exports.boomerang, exports.drone, exports.testbed9]
 //exports.testbed8.UPGRADES_TIER_1 = [exports.sunchip, exports.autosunchip, exports.invissunchip, exports.gunchip, exports.missile, exports.twistmissile, exports.hypermissile, exports.snake, exports.hive]
 
-exports.centre.UPGRADES_TIER_1 = [exports.centre2,exports.centre3,exports.centre4,exports.centre5]
+exports.testbed9.UPGRADES_TIER_1 = [exports.centre,exports.centre2,exports.centre3,exports.centre4,exports.centre5]
 
 
 exports.basic.UPGRADES_TIER_1 = [exports.twin, exports.sniper, exports.machine, exports.flank, exports.director, exports.pound, exports.trapper];
