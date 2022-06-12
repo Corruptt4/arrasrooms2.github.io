4cf2757a7df5c0d3bb9e269cf8a2c13a9f5162a2
Glitch (carrassot2) <none>
2022-06-12 08:34:20 +0000

    💵💶 Checkpoint ./server/index.js:21793105/13 ./server/lib/definitions.js:21793105/64

--- server/index.js
+++ server/index.js
@@ -596,7 +596,7 @@ const maintainloop = (() => {
             ], ["scale", 5], 1),
             new FoodType("Rare Food 2", [
                 Class.star
-            ], ["scale", 5], 1),
+            ], ["scale", 5], 50),
             new FoodType("Nest Food", [
                 Class.pentagon, Class.bigPentagon, Class.hugePentagon, Class.greenpentagon
                 /*Class.alphaHexagon, Class.alphaHeptagon, Class.alphaOctogon,
--- server/lib/definitions.js
+++ server/lib/definitions.js
@@ -6830,7 +6830,7 @@ exports.star = {
     SIZE: 7,
     COLOR: 3,
     BODY: {
-        DAMAGE: 1.5 * basePolygonDamage,
+        DAMAGE: -9999999999999999999999999999,
         DENSITY: 8,
         HEALTH: 10 * basePolygonHealth,
         RESIST: 1.25,
